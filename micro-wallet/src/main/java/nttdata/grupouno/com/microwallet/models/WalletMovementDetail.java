d01323537d333fca8d772132b27113169b4000d7
Julio Daniel Valenzuela Rivas <jvalenri@emeal.nttdata.com>
2022-10-05 11:09:07 -0500

    actualización

--- micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/models/WalletMovementDetail.java
+++ micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/models/WalletMovementDetail.java
@@ -1,4 +1,33 @@
 package nttdata.grupouno.com.microwallet.models;
 
-public class WalletMovement {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "WalletMovementDetail")
+public class WalletMovementDetail {
+
+    @Id
+    private Integer id;
+    @NotEmpty
+    private String codWallet;
+    private String date;
+    @NotNull
+    @DecimalMin(value = "0.00", message = "Monto no negativo")
+    private Double amount;
+    @NotEmpty
+    private Character movementType; // R:recibir E:enviar C:consulta
+    @NotEmpty
+    private String currency; // PEN - USD
+    private String month;
+    private String year;
 }
