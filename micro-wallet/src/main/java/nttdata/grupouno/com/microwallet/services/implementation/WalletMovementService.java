6147e23425580a3c8de02007ddc8802cc1589a75
Julio Daniel Valenzuela Rivas <jvalenri@emeal.nttdata.com>
2022-10-05 17:02:24 -0500

    actualización wallet

--- micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/services/implementation/WalletMovementService.java
+++ micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/services/implementation/WalletMovementService.java
@@ -7,46 +7,41 @@ import nttdata.grupouno.com.microwallet.repositories.IClientWalletRepositories;
 import nttdata.grupouno.com.microwallet.repositories.IWalletMovementRepository;
 import nttdata.grupouno.com.microwallet.services.IClientWalletService;
 import nttdata.grupouno.com.microwallet.services.IWalletMovementService;
+import nttdata.grupouno.com.microwallet.util.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Service
 public class WalletMovementService implements IWalletMovementService {
     @Autowired
     private IWalletMovementRepository IWalletMovementRepository;
-    @Autowired
-<<<<<<< HEAD
-    private IWalletRepository IWalletRepository;
 
     @Autowired
-=======
->>>>>>> bbefdc6efc3f98ad28e87b8a483f09d94763abb7
     private IClientWalletRepositories clientWalletRepositories;
 
     @Override
     public Mono<WalletMovementModel> registerMovement(WalletMovementDto walletMovementDto) {
-<<<<<<< HEAD
-        return clientWalletRepositories.findByNumberPhone(walletMovementDto.getNumberPhone())
+                return clientWalletRepositories.findByNumberPhone(walletMovementDto.getNumberPhone())
                 .flatMap(x -> {
-                     return IWalletRepository.findByCodCliente(x.getId()).flatMap(y -> {
-                         if(walletMovementDto.getMovementType().equals("E")){
-=======
-        return clientWalletRepositories.findByNumberPhone(walletMovementDto.getCelular())
-                .flatMap(x -> {
+                    double newAmount = 0.0;
                     if(walletMovementDto.getMovementType().equals("E")){
-                        return clientWalletRepositories.findById(x.getId()).flatMap(y -> {
->>>>>>> bbefdc6efc3f98ad28e87b8a483f09d94763abb7
-                            if(y.getAmount()<walletMovementDto.getAmount()){
-                                return Mono.empty();
-                            }
-                         }
-                         return Mono.just(y);
-                        }).switchIfEmpty(Mono.empty());
+                        if(x.getAmount()<walletMovementDto.getAmount()){
+                            //saldo insuficiente para pago
+                            return Mono.empty();
+                        }
+                        newAmount = x.getAmount() - walletMovementDto.getAmount();
+                        return Mono.just(x);
+                    }
+                    else{
+                        newAmount  = x.getAmount() + walletMovementDto.getAmount();
+                    }
+                    x.setAmount(newAmount);
+                    return Mono.just(x);
                 })
-                .map(x -> new ClientWalletModel(null,null,null,null,null,null))
                 .flatMap(x -> clientWalletRepositories.save(x))
                 .map(x ->  new WalletMovementModel(UUID.randomUUID().toString(), x.getId() , Util.dateTimeToString(new Date()), walletMovementDto.getAmount(), walletMovementDto.getMovementType(), walletMovementDto.getCurrency(), Util.getMonth(new Date()), Util.getYear(new Date()) ))
                 .flatMap(x -> IWalletMovementRepository.save(x))
