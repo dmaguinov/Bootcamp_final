6f7a4b0165423c6b5fbeb8a5ab9069bcf258616d
Julio Daniel Valenzuela Rivas <jvalenri@emeal.nttdata.com>
2022-10-05 13:43:37 -0500

    actualización wallet

--- micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/models/WalletModel.java
+++ micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/models/WalletModel.java
@@ -3,11 +3,19 @@ package nttdata.grupouno.com.microwallet.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "Wallet")
 public class Wallet {
+    @Id
+    private Integer id;
+    @NotEmpty
+    private String codClienteMovil;
 }
+
