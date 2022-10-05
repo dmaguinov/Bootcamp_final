3cedc9327acf442050b1a23fa88da58c4f960736
Francisco Frank CY <108429033+FranciscoCY@users.noreply.github.com>
2022-10-05 09:43:49 -0500

    Micro wallet

---
+++ micro-wallet/src/main/java/nttdata/grupouno/com/microwallet/MovilWalletApplication.java
@@ -0,0 +1,13 @@
+package nttdata.grupouno.com.microwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovilWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovilWalletApplication.class, args);
+	}
+
+}
