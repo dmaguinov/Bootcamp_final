586b6abcec2bb77d26e6e3a30de5b2a7207294e0
asolanot94 <andresolano159@gmail.com>
2022-10-06 16:42:15 -0500

    kafka-clientes

--- Operaciones/src/main/java/nttdata/grupouno/com/operations/config/KafkaProducerConfig.java
+++ Operaciones/src/main/java/nttdata/grupouno/com/operations/config/KafkaProducerConfig.java
@@ -1,6 +1,6 @@
 package nttdata.grupouno.com.operations.config;
 
-import nttdata.grupouno.com.operations.events.Event;
+import nttdata.grupouno.com.operations.models.MasterAccountModel;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -10,7 +10,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,10 +17,10 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
-    private final String bootstrapAddress = "localhost:9091";
+    private final String bootstrapAddress = "localhost:9092";
 
     @Bean
-    public ProducerFactory<String, Mono<Event<?>>> producerFactory(){
+    public ProducerFactory<String, MasterAccountModel> producerFactory(){
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapAddress);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -31,8 +30,8 @@ public class KafkaProducerConfig {
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
-    @Bean(name = "kafkaAccountTemplate")
-    public KafkaTemplate<String, Mono<Event<?>>> kafkaTemplate() {
+    @Bean(name = "kafkaProducerTemplate")
+    public KafkaTemplate<String, MasterAccountModel> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 }
